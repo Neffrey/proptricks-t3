19d44a2daaa9d5f008c4fad7cc7d1c3e83347eb8
Neffrey <neffreyl@gmail.com>
2022-12-13 07:42:41 -0600

    Planetscale db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,10 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
+
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
+import { prisma } from "server/db/client";
+import { env } from "env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -19,12 +19,21 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID ? env.GOOGLE_CLIENT_ID : "",
+      clientSecret: env.GOOGLE_CLIENT_SECRET ? env.GOOGLE_CLIENT_SECRET : "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
-    // ...add more providers here
   ],
+  theme: {
+    colorScheme: "dark",
+  },
 };
 
 export default NextAuth(authOptions);
