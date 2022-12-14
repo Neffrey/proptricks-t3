2184d3b9cfd8fc994367841e9fd03c0bbe39be66
Neffrey <neffreyl@gmail.com>
2022-12-14 06:24:41 -0600

    Install DaisyUI & themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      "night",
+      "light",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "forest",
+      "aqua",
+      "pastel",
+      "fantasy",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "coffee",
+      "winter",
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
