92a53b0cf2253690370f0f49d83d40c713f59b6b
Neffrey <neffreyl@gmail.com>
2022-12-15 13:44:37 -0600

    tricks

--- src/pages/tricks.tsx
+++ src/pages/tricks.tsx
@@ -11,15 +11,15 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Add a Trick</title>
+        <title>Tricks</title>
         <meta name="description" content="Upload a trick of your own" />
         <link rel="icon" href="/CircleLogoIco.ico" />
       </Head>
 
       <main className="min-w-screen flex min-h-screen w-full flex-col items-center justify-center">
         <h1 className="text-5xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
-          Add a Trick
-        </h1>
+          Tricks
+        </h1> 
       </main>
     </>
   );
