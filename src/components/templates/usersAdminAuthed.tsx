551884e36ff9920ca7fbc371ddbdcea92e8d59b1
Neffrey <neffreyl@gmail.com>
2023-01-04 14:01:35 -0600

    remove old users table from AdminAuthed

--- src/components/templates/usersAdminAuthed.tsx
+++ src/components/templates/usersAdminAuthed.tsx
@@ -1,12 +1,10 @@
-// COMPONENTS
-import AllUsersTable from "components/molecules/allUsersTable";
 
 // FC
 const UsersAdminAuthed = () => {
   // RETURN
   return (
     <div className="flex min-h-screen w-full flex-col p-8 text-center text-xl">
-      <AllUsersTable />
+      Congratz youre an admin
     </div>
   );
 };
