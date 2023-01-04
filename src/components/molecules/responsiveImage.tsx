1228417a18077df10260d59ece3685e9b33b8efb
Neffrey <neffreyl@gmail.com>
2023-01-04 12:19:58 -0600

    Update responsiveImage.tsx

--- src/components/molecules/responsiveImage.tsx
+++ src/components/molecules/responsiveImage.tsx
@@ -6,53 +6,42 @@ import Image from "next/image";
 import addClassName from "components/helpers/addClassName";
 
 // PROPS
+type ObjectFitEnum = "contain" | "cover" | "fill" | "none" | "scale-down";
 interface responsiveImageProps {
-  aspectRatio: string;
   alt: string;
-  className?: string;
   src: string;
-  unit?: string;
-  width: number;
+  className?: string;
+  width?: number;
+  height?: number;
+  objectFit?: ObjectFitEnum | undefined;
+  parentPosition?: string;
 }
 
 // FC
 const ResponsiveImage = ({
-  aspectRatio,
   alt,
   className = "",
   src,
-  unit = "",
-  width,
+  width = 0,
+  height = 0,
+  objectFit,
+  parentPosition = "relative",
 }: responsiveImageProps) => {
-  // aspectRatio MUST have a / in its string
-  const aspectX =
-    aspectRatio.indexOf("/") >= 0
-      ? parseInt(aspectRatio.slice(0, aspectRatio.indexOf("/")))
-      : 0;
-  const aspectY =
-    aspectRatio.indexOf("/") >= 0
-      ? parseInt(aspectRatio.slice(aspectRatio.indexOf("/") + 1))
-      : 0;
-
-  const floorTo2Dec = (number: number) => {
-    return Math.floor(number * 100) / 100;
-  };
-
-  const calcHeight = () => {
-    return floorTo2Dec((aspectY / aspectX) * width).toString() + unit;
-  };
-  const calcWidth = () => {
-    return floorTo2Dec(width).toString() + unit;
-  };
-
   return (
-    <div className={"block h-full w-full" + addClassName(className)}>
+    <div
+      className={
+        "block h-full w-full" +
+        addClassName(className) +
+        (parentPosition ? " relative" : "") +
+        (objectFit === "cover" ? " overflow-hidden" : "")
+      }
+    >
       <Image
         src={src}
         alt={alt}
-        layout="responsive"
-        width={calcWidth()}
-        height={calcHeight()}
+        width={width}
+        height={height}
+        style={objectFit ? { objectFit: objectFit } : undefined}
       />
     </div>
   );
